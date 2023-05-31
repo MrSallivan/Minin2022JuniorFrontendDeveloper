85e9ad912a86df4d84f901152a9e89ebe6331440
MrSallivan <nik.sallivan@gmail.com>
2023-06-01 07:29:21 +1000

    rollup

--- lesson18-Webpack-rolup/rollup.config.mjs
+++ lesson18-Webpack-rolup/rollup.config.mjs
@@ -1,6 +1,10 @@
 // rollup.config.js
 
 import { babel } from '@rollup/plugin-babel'
+import styles from 'rollup-plugin-styles'
+import image from 'rollup-plugin-img'
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
 
 export default {
 	input: './src/index.js',
@@ -9,6 +13,14 @@ export default {
 		format: 'iife'
 	},
 	plugins: [
-		babel({ babelHelpers: 'bundled' })
+		babel({ babelHelpers: 'bundled' }),
+		styles(),
+		image({ limit: 10000 }),
+		serve({
+			open: true,
+			contentBase: './',
+			port: 8000,
+		}),
+		livereload(),
 	]
 };
