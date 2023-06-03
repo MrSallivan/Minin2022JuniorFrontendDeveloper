95d7897adc75e3e98628305e0e83e07895ff21fb
MrSallivan <nik.sallivan@gmail.com>
2023-06-03 14:54:32 +1000

    endWebpack

--- lesson18.1-HW-Webpack/src/index.js
+++ lesson18.1-HW-Webpack/src/index.js
@@ -1,1 +1,7 @@
-console.log('HelloWorld');
+import './../style.css'
+import JS_IMAGE from '../assets/js.png'
+
+const jsImageHTML = document.createElement('img')
+jsImageHTML.className = `js-image`
+jsImageHTML.src = JS_IMAGE
+document.body.append(jsImageHTML)
--- lesson18.1-HW-Webpack/webpack.config.js
+++ lesson18.1-HW-Webpack/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
@@ -7,4 +9,44 @@ module.exports = {
 		filename: 'build.js',
 		path: path.resolve(__dirname, 'build'),
 	},
+	devServer: {
+		open: true,
+		compress: true,
+		port: 4000,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							['@babel/preset-env', { targets: "defaults" }]
+						]
+					}
+				}
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+					},
+				],
+			},
+
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html'
+		}),
+		new CleanWebpackPlugin(),
+	]
 };
