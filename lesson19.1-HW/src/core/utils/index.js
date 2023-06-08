ca07dda1ca7b77f3972ae70cb286661ad8d5c6e5
MrSallivan <nik.sallivan@gmail.com>
2023-06-08 18:03:13 +1000

    19.6

---
+++ lesson19.1-HW/src/core/utils/index.js
@@ -0,0 +1,6 @@
+export const calculateSumOfNumbers = (arrNums) => {
+	return arrNums.reduce((acc, item) => acc += item, 0)
+}
+export const getFormattedTime = (date) => {
+
+}
--- lesson19.1-HW/src/modules/App.js
+++ lesson19.1-HW/src/modules/App.js
@@ -3,13 +3,6 @@ import { DonateList } from './donate-list'
 
 
 
-// const mockDonates = [
-// 	{ amount: 4, date: new Date() },
-// 	{ amount: 20, date: new Date() },
-// 	{ amount: 3, date: new Date() },
-// 	{ amount: 1, date: new Date() },
-// ]
-
 export default class App {
 	#donateForm
 	#donateList
