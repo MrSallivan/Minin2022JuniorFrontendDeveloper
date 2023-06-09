242a61e8ff3d4dd9865979dc3c6911dd35bbc5c4
MrSallivan <nik.sallivan@gmail.com>
2023-06-09 16:38:04 +1000

    react

---
+++ React/1Intro/my-app/src/App.js
@@ -0,0 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.css'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Исправь<code>src/App.js</code> и сохрани для изменений.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
