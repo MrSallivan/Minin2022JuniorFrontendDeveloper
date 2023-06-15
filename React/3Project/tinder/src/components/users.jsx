ce1f4f5643a0f97f8634954069d60ba14732fdd5
MrSallivan <nik.sallivan@gmail.com>
2023-06-15 19:30:42 +1000

    project

--- React/3Project/tinder/src/components/users.jsx
+++ React/3Project/tinder/src/components/users.jsx
@@ -1,9 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
 import api from "../api"
 
 const Users = () => {
-	console.log(api.users.fetchAll());
-	return <h1>drom .ru</h1>
+
+	const [users, setUsers] = useState(api.users.fetchAll())
+	const handleDelete = (userId) => { }
+	const renderPhrase = (number) => { }
+
+	return (
+		<>
+			<span className={"badge bg-" + (users.length > 0 ? "primary" : "danger")}>13 дебилов в сети</span>
+			<table className="table">
+				<thead>
+					<tr>
+						<th scope="col">Имя</th>
+						<th scope="col">Качества</th>
+						<th scope="col">Профессия</th>
+						<th scope="col">Встретился, раз</th>
+						<th scope="col">Оценка</th>
+						<th />
+					</tr>
+				</thead>
+				<tbody>
+					{users.map((user) => (
+						<tr key={user._id}>
+							<td>{user.name}</td>
+							<td>{user.qualities.map((item) => (<span className={"badge m-1 bg-" + item.color} key={item._id}>{item.name}</span>))}</td>
+							<td>{user.profession.name}</td>
+							<td>{user.completedMeetings}</td>
+							<td>{user.rate}</td>
+							<td><button type="button" className="btn btn-danger">delete</button></td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</>
+	)
 }
 
 export default Users
--- React/3Project/tinder/src/index.js
+++ React/3Project/tinder/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import Users from './components/users'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
