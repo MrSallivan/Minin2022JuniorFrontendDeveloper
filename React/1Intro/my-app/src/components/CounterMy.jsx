2daa545e6277316a3d71468b4275a6607a4e4429
MrSallivan <nik.sallivan@gmail.com>
2023-06-17 07:34:30 +1000

    8

--- React/1Intro/my-app/src/components/CounterMy.jsx
+++ React/1Intro/my-app/src/components/CounterMy.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from "react"
 
-const CounterMy = () => {
-	let [count, setCount] = useState(0)
+const CounterMy = (props) => {
+
+	let [value, setValue] = useState(props.value)
 	// const [tags, setTags] = useState(['tag1', 'tag2', 'tag3'])
 
-	const formatCount = () => {
-		return count === 0 ? 'empty' : count
+	const formatValue = () => {
+		return value === 0 ? 'empty' : value
 	}
 	const increment = () => {
-		setCount((prevState) => prevState + 1)
+		setValue((prevState) => prevState + 1)
 	}
 	const decrement = () => {
-		setCount((prevState) => prevState - 1)
+		setValue((prevState) => prevState - 1)
 	}
 	const bgBadge = () => {
 		const badgeClass = `badge text-bg-primary m-2`
 		const badgeClassEmpty = `badge text-bg-warning m-2`
-		return count === 0 ? badgeClassEmpty : badgeClass
+		return value === 0 ? badgeClassEmpty : badgeClass
 	}
 	// const handleTags = (id) => {
 	// 	setTags((prevState) => prevState.filter(item => id !== item))
@@ -36,13 +37,13 @@ const CounterMy = () => {
 	// }
 
 	return (
-
 		<div>
-			<span className={bgBadge()} >{formatCount()}</span>
+			<span>{props.name}</span>
+			<span className={bgBadge()} >{formatValue()}</span>
 			<button className="btn btn-primary btn-sm m-2" onClick={increment}>+</button>
 			<button className="btn btn-primary btn-sm m-2" onClick={decrement}>-</button>
-		</div>
-	)
+			<button className="btn btn-danger btn-sm m-2" onClick={props.onDelete}>Delete</button>
+		</div>)
+
 }
 export default CounterMy
- 
--- React/1Intro/my-app/src/components/countersList.jsx
+++ React/1Intro/my-app/src/components/countersList.jsx
@@ -1,16 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import CounterMy from "./CounterMy"
 
 const CountersList = () => {
-	const counters = [
-		{ id: 0, value: 0 }, 
-		{ id: 1, value: 4 }, 
-		{ id: 2, value: 8 }
-	]
-
+	const [counters, setCounters] = useState([
+		{ id: 0, value: 5, name: 'ненужная вещь' },
+		{ id: 1, value: 0, name: 'ложка' },
+		{ id: 2, value: 8, name: 'вилка' },
+		{ id: 3, value: 8, name: 'тарелка' },
+		{ id: 4, value: 8, name: 'набор минималиста' },
+	])
+	const handleDelete = () => {
+		console.log('handele delete');
+	}
 	return (
 		<>
-			{counters.map(counter => <CounterMy key={counter.id} />)}
+			{counters.map(counter =>
+				<CounterMy key={counter.id} value={counter.value} name={counter.name} onDelete={handleDelete} />
+			)}
 		</>)
 }
 
