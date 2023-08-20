ff77381930b993dab1290237563fdad4507636ab
MrSallivan <nik.sallivan@gmail.com>
2023-08-20 22:14:21 +1000

    delete all

--- React/3Project/tinder/src/app/App.js
+++ React/3Project/tinder/src/app/App.js
@@ -3,37 +3,7 @@ import api from "./api"
 import Users from "./components/users"
 
 const App = () => {
-  const [users, setUsers] = useState()
-  useEffect(() => {
-    api.users.fetchAll().then((data) => setUsers(data))
-  }, [])
-
-  const handleDelete = (userId) => {
-    setUsers(users.filter((user) => user._id !== userId))
-  }
-
-  const handleToggleBookMark = (id) => {
-    setUsers(
-      users.map((user) => {
-        if (user._id === id) {
-          return { ...user, bookmark: !user.bookmark }
-        }
-        return user
-      })
-    )
-  }
-
-  return (
-    <>
-      {users && (
-        <Users
-          onDelete={handleDelete}
-          onToggleBookMark={handleToggleBookMark}
-          users={users}
-        />
-      )}
-    </>
-  )
+  
 }
 
 export default App
