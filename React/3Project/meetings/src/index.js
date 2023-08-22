9f98cd6be859e029941425e95372113aa1bee142
MrSallivan <nik.sallivan@gmail.com>
2023-08-22 22:28:25 +1000

    22.08

--- React/3Project/meetings/src/index.js
+++ React/3Project/meetings/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import "bootstrap/dist/css/bootstrap.css";
-import App from "./app/App";
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import "bootstrap/dist/css/bootstrap.css"
+import App from "./app/App"
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+const root = createRoot(document.querySelector("#root"))
+root.render(<App />)
