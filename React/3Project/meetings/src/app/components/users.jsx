103a07b89dc58ce450f4ced302073f307c187976
MrSallivan <nik.sallivan@gmail.com>
2023-08-23 19:44:33 +1000

    111

--- React/3Project/meetings/src/app/components/users.jsx
+++ React/3Project/meetings/src/app/components/users.jsx
@@ -1,30 +1,42 @@
 import React from "react";
 import User from "./user";
+import Pagination from "./pagination";
+
 const Users = ({ users, ...rest }) => {
+	const count = users.length
+	const pageSize = 4
+	const handlePageChange = (pageIndex) => {
+		console.log("page", pageIndex);
+	}
     return (
-        <>
-            {users.length > 0 && (
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Имя</th>
-                            <th scope="col">Качества</th>
-                            <th scope="col">Провфессия</th>
-                            <th scope="col">Встретился, раз</th>
-                            <th scope="col">Оценка</th>
-                            <th scope="col">Избранное</th>
-                            <th />
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map((user) => (
-                            <User key={user._id} {...rest} {...user} />
-                        ))}
-                    </tbody>
-                </table>
-            )}
-        </>
-    );
+      <>
+        {count > 0 && (
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Имя</th>
+                <th scope="col">Качества</th>
+                <th scope="col">Провфессия</th>
+                <th scope="col">Встретился, раз</th>
+                <th scope="col">Оценка</th>
+                <th scope="col">Избранное</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <User key={user._id} {...rest} {...user} />
+              ))}
+            </tbody>
+          </table>
+        )}
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          onPageChange={handlePageChange}
+        />
+      </>
+    )
 };
 
 export default Users;
