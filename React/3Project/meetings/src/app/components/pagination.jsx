ef5b0820d224877f6d19aca059b5bb833383df26
MrSallivan <nik.sallivan@gmail.com>
2023-08-24 18:08:37 +1000

    24.08

--- React/3Project/meetings/src/app/components/pagination.jsx
+++ React/3Project/meetings/src/app/components/pagination.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
-const Pagination = (props) => {
-	return ( 
-		<>
-		</>
-	 );
+const Pagination = ({ itemsCount, pageSize, onPageChange }) => {
+	console.log(itemsCount, pageSize, onPageChange );
+	let pages = pageSize/itemsCount
+ let numbersPages = pages % 1 === 0 pages : pages + 1
+ console.log(numbersPages);
+  return (
+    <nav aria-label="...">
+      <ul class="pagination pagination-sm">
+				
+        <li class="page-item active" aria-current="page">
+          <span class="page-link">1</span>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">
+            2
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">
+            3
+          </a>
+        </li>
+      </ul>
+    </nav>
+  )
 }
  
 export default Pagination;
