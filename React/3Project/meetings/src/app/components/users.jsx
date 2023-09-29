c0632f1787a5e45de24192c9c6b3fdc1339e2b69
MrSallivan <nik.sallivan@gmail.com>
2023-09-29 23:25:40 +1000

    start develop sorting block

--- React/3Project/meetings/src/app/components/users.jsx
+++ React/3Project/meetings/src/app/components/users.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
-import User from "./user"
 import Pagination from "./pagination"
 import paginate from "../utils/paginate"
 import GroupList from "./groupList"
 import API from "../api"
 import SearchStatus from "./searchStatus"
+import UserTable from "./usersTable"
 
 const Users = ({ users, ...rest }) => {
   const [currentPage, setCurrentPage] = useState(1)
@@ -58,29 +58,9 @@ const Users = ({ users, ...rest }) => {
           </button>
         </div>
       )}
-
       <div className="d-flex flex-column">
         <SearchStatus length={count} />
-        {count > 0 && (
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Имя</th>
-                <th scope="col">Качества</th>
-                <th scope="col">Провфессия</th>
-                <th scope="col">Встретился, раз</th>
-                <th scope="col">Оценка</th>
-                <th scope="col">Избранное</th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              {userCrop.map((user) => (
-                <User key={user._id} {...rest} {...user} />
-              ))}
-            </tbody>
-          </table>
-        )}
+        {count > 0 && <UserTable users={userCrop} {...rest} />}
         <div className="d-flex justify-content-center">
           <Pagination
             itemsCount={count}
